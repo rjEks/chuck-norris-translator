e07fd62d5712f2c0fff6836ed193c7cddd543002
rjeks <rjekstein@gmail.com>
2022-08-09 21:26:51 -0300

    adjust workflow

--- src/chk.py
+++ src/chk.py
@@ -5,6 +5,5 @@ import boto3
 def return_joke():
     
     chk_endpoint_response = requests.get("https://api.chucknorris.io/jokes/random")
-    chk_endpoint_response_json = chk_endpoint_response.json()
-    
+    chk_endpoint_response_json = chk_endpoint_response.json()    
     return chk_endpoint_response_json["value"]
