efc906ee128318f5578250f78dbe00afdd102cfe
rjeks <rjekstein@gmail.com>
2022-08-11 14:33:35 -0300

    adjust cfn for deploy

---
+++ tweet.py
@@ -0,0 +1,26 @@
+import json
+import requests
+import tweepy  as tw 
+import os
+import re
+
+def set_credentials(secrets):
+
+    bearer_token=secrets['BEARER_TOKEN']
+    access_token = secrets['ACCESS_TOKEN']
+    access_token_secret = secrets['ACCESS_TOKEN_SECRET']
+    api_key = secrets['API_KEY']
+    api_key_secret = secrets['API_KEY_SECRET']
+    
+    return bearer_token,api_key,api_key_secret,access_token,access_token_secret
+
+def getClient(bearer_token,api_key,api_key_secret,access_token,
+            access_token_secret):    
+
+    client = tw.Client(bearer_token=bearer_token, 
+                       consumer_key=api_key, 
+                       consumer_secret=api_key_secret, 
+                       access_token=access_token, 
+                       access_token_secret=access_token_secret )
+    return client
+    
