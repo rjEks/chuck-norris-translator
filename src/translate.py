ed083f138acd366e9a972667f6e950ac056fa6ec
rjeks <rjekstein@gmail.com>
2022-08-11 15:27:08 -0300

    adjust function name

--- src/translate.py
+++ src/translate.py
@@ -5,8 +5,8 @@ def translate(text, client, source_language, target_language):
     
     translate_response = client.translate_text(
         Text=text,
-        SourceLanguageCode='source_language',
-        TargetLanguageCode='target_language'
+        SourceLanguageCode=source_language,
+        TargetLanguageCode=target_language
     )
     
     return translate_response
